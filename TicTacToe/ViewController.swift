77d8fe706b15ac679ed47506cfb0f612aaff8f23
Vsevolod <vsevolod.lashin@gmail.com>
2023-03-24 04:37:50 +0300

    Final class added

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet var ticTacToeLabel: UILabel!
     @IBOutlet var resultOfGameButton: UILabel!
